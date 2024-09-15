f2309551c694d4ffcff0aab18d7a643655ad5816
Shobha Jangade <jangadeshobha@gmail.com>
2024-09-15 19:57:22 +0530

    Create Sphere.c

---
+++ C/Sphere.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   float r;
+   printf("enter value of r : ");
+   scanf("%f",&r);
+   float sphere=4*3.14*r*r;
+   printf("area of sphere is %f ",sphere);
+   return 0;
+}
