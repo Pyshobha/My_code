218f8492bb0fe1f434322ecb221e911db7d129ae
Shobha Jangade <jangadeshobha@gmail.com>
2024-09-15 19:51:12 +0530

    Create sum.c

---
+++ C/sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("enter first no : ");
+    scanf("%d",&x);
+    int y;
+    printf("enter second no : ");
+    scanf("%d",&y);
+    int sum=x+y;
+    printf("the sum is %d",sum);
+    return 0;
+}
