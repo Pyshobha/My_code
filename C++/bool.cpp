6f57a46fc0937f9e73d0ddd3b92fadcc99959805
Shobha Jangade <jangadeshobha@gmail.com>
2025-01-24 20:00:38 +0530

    Create bool.cpp

---
+++ C++/bool.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool a=true,b=false,c=true,d=false;
+    cout<<a<<b<<c<<d; //if a bool data type is true then return 1 and if false then return 0
+
+}
