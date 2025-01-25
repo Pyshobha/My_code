f41b17b12254b615e80322ac0e5f666ac6cdcbbd
Shobha Jangade <jangadeshobha@gmail.com>
2025-01-25 17:45:05 +0530

    Create while_loop.cpp

---
+++ C++/while_loop.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    int i=1;
+    while(i<=10){
+        cout<<i<<endl;
+        i++;
+
+    }
+    return 0;
+}
+// multiplication of 6
+// #include<iostream>
+// using namespace std;
+//  int main(){
+//     int i=1;
+//     while(i<=10){
+//         cout<<i*6<<endl;
+//         i++;
+
+//     }
+//     return 0;
+// }
+
+//****** second way
+// multiplication of 6
+// #include<iostream>
+// using namespace std;
+//  int main(){
+//     int i=1;
+//     while(i<=10){
+//         cout<<i*60/10<<endl;
+//         i++;
+
+//     }
+//     return 0;
+// }
