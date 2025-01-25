03a44d36ccef23d8a037ef83dba58b579f4932bb
Shobha Jangade <jangadeshobha@gmail.com>
2025-01-25 17:48:57 +0530

    Create do_while_loop.cpp

---
+++ C++/do_while_loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+// there is do while and while loop is difference is that if do while is false then execute once time but in while loop no execution 
+// means not print anything
+int i=1;
+do
+{
+   cout<<i<<endl;
+   i++;
+} while(i<=10);
+return 0;
+}
