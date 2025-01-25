95ea76781ecaefdc419baa2c21b1cbf53558ff41
Shobha Jangade <jangadeshobha@gmail.com>
2025-01-25 16:43:54 +0530

    Create if_control.cpp

---
+++ C++/if_control.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=0;
+    if(i<3){  // there are answer give only 0 not 0,1,2 because if statement is execute only one time 
+    // not many time if contorl statement is not a loop 
+        cout<<i;
+        i++;
+    }
+    cout<<endl<<i;
+}
